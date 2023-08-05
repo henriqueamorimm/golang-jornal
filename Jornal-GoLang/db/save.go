fb566514f3cc6b3da245abb8af34c447e48ad46f
Aligg <114702742+henriqueamorimm@users.noreply.github.com>
2023-08-05 14:28:38 -0300

    Update save.go

--- Jornal-GoLang/db/save.go
+++ Jornal-GoLang/db/save.go
@@ -1,12 +1,21 @@
 package db
 
+import (
+	"context"
+	"fmt"
+
+)
+
 func Insert(collection string, data interface{}) error {
 	client, ctx := getConnection()
 	defer client.Disconnect(ctx)
 
-	c := client.Database("webform").Collection(collection) 
+	c := client.Database("golang_jornal").Collection(collection)
 
-	_, err := c.InsertOne(ctx, data) 
+	_, err := c.InsertOne(context.Background(), data)
+	if err != nil {
+		return fmt.Errorf("erro ao inserir dados: %v", err)
+	}
 
-	return err
+	return nil
 }
